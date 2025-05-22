93825014e64b5c78f561a1132922ba71dcffb6cd
guiFerranti <guilhermecousseau@icloud.com>
2025-05-22 19:10:13 -0300

    feat: configuracao basica de testes unitarios

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,15 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication, MockeryPHPUnitIntegration;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->artisan('migrate:fresh');
+    }
 }
