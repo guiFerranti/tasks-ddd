f2fc21c187257c83a5b8ec33fbbba9d28e39058d
guiFerranti <guilhermecousseau@icloud.com>
2025-05-22 13:34:32 -0300

    fix: ajustando ordem de rotas para evitar conflito

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,21 @@ Route::prefix('users')->group(function () {
 
 // auth routes
 Route::middleware('jwt.auth')->group(function () {
+    // admin routes
+    Route::middleware('admin')->group(function () {
+        // users
+        Route::prefix('users')->group(function () {
+            Route::get('/', [UserController::class, 'index']);
+            Route::delete('/{id}', [UserController::class, 'destroy']);
+        });
+
+
+        Route::prefix('tasks')->group(function () {
+            Route::delete('/{task}', [TaskController::class, 'destroy']);
+            Route::get('/deleted', [TaskController::class, 'indexDeleted']);
+        });
+    });
+
     // logout
     Route::prefix('auth')->group(function () {
         Route::post('/logout', [AuthController::class, 'logout']);
@@ -35,20 +50,5 @@ Route::middleware('jwt.auth')->group(function () {
         Route::post('/', [TaskController::class, 'store']);
         Route::get('/{task}', [TaskController::class, 'show']);
         Route::put('/{task}', [TaskController::class, 'update']);
-    });
-
-    // admin routes
-    Route::middleware('admin')->group(function () {
-        // users
-        Route::prefix('users')->group(function () {
-            Route::get('/', [UserController::class, 'index']);
-            Route::delete('/{id}', [UserController::class, 'destroy']);
-        });
-
-
-        Route::prefix('tasks')->group(function () {
-            Route::delete('/{task}', [TaskController::class, 'destroy']);
-            Route::get('/deleted', [TaskController::class, 'indexDeleted']);
-        });
     });
 });
